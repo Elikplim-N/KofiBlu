5eb791b1cc160f6c2ce8f2776aaef16d0b66e0b5
Elikplim Kofi Nyahe <ghnewton10@gmail.com>
2025-10-16 02:15:21 +0000

    Try fixing this error: `Console Error: Invalid DOM property `http-equiv`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
-        <meta http-equiv="Permissions-Policy" content="bluetooth=*" />
+        <meta httpEquiv="Permissions-Policy" content="bluetooth=*" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet" />
